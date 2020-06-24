42a64459842461eb8094f10c1070233fff4f0799
Piotrek1697 <30784285+Piotrek1697@users.noreply.github.com>
2020-06-24 22:30:48 +0200

    Initial commit

---
+++ app/src/main/java/com/example/pillsrecognition/MainActivity.kt
@@ -0,0 +1,109 @@
+package com.example.pillsrecognition
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.view.Surface.ROTATION_90
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata
+import com.google.firebase.ml.vision.objects.FirebaseVisionObject
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.label.ImageLabeling
+import com.google.mlkit.vision.label.automl.AutoMLImageLabelerLocalModel
+import com.google.mlkit.vision.label.automl.AutoMLImageLabelerOptions
+import com.otaliastudios.cameraview.Frame
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), 101)
+
+        } else {
+            cameraView!!.setLifecycleOwner(this)
+
+
+            cameraView!!.addFrameProcessor {
+                extractDataFromFrame(it!!) { result ->
+                    text_view.text = result
+                }
+            }
+        }
+
+
+    }
+
+    private fun extractDataFromFrame(frame: Frame, callback: (String) -> Unit) {
+
+        val localModel = AutoMLImageLabelerLocalModel.Builder()
+            .setAssetFilePath("manifest.json")
+            // or .setAbsoluteFilePath(absolute file path to manifest file)
+            .build()
+
+        val autoMLImageLabelerOptions = AutoMLImageLabelerOptions.Builder(localModel)
+            .setConfidenceThreshold(0.5f)  // Evaluate your model in the Firebase console
+            // to determine an appropriate value.
+            .build()
+        val labeler = ImageLabeling.getClient(autoMLImageLabelerOptions)
+        labeler.process(getVisionImageFromFrame(frame))
+            .addOnSuccessListener {labels ->
+                for (label in labels) {
+                    val text = label.text
+                    val confidence = label.confidence
+                    val index = label.index
+                    Log.d("LabelTag", text)
+                    Log.d("LabelTag", confidence.toString())
+                    Log.d("LabelTag", index.toString())
+                }
+            }.addOnFailureListener {e ->
+
+            }
+
+    }
+
+    private fun getVisionImageFromFrame(frame: Frame): InputImage {
+        val data: ByteArray = frame?.data
+        val imageMetaData = FirebaseVisionImageMetadata.Builder()
+            .setFormat(FirebaseVisionImageMetadata.IMAGE_FORMAT_NV21)
+            .setRotation(FirebaseVisionImageMetadata.ROTATION_90)
+            .setHeight(frame.size.height)
+            .setWidth(frame.size.width)
+            .build()
+
+        val image = InputImage.fromByteArray(
+            data,
+            /* image width */ 480,
+            /* image height */ 360,
+            ROTATION_90,
+            InputImage.IMAGE_FORMAT_NV21
+        );// or IMAGE_FORMAT_YV12)
+
+        return image
+    }
+
+}
+
+class DrawingView(context: Context, var visionObjects: List<FirebaseVisionObject>) : View(context) {
+
+    companion object {
+        val categoryNames: Map<Int, String> = mapOf(
+            FirebaseVisionObject.CATEGORY_UNKNOWN to "Unknown",
+            FirebaseVisionObject.CATEGORY_HOME_GOOD to "Home Goods",
+            FirebaseVisionObject.CATEGORY_FASHION_GOOD to "Fashion Goods",
+            FirebaseVisionObject.CATEGORY_FOOD to "Food",
+            FirebaseVisionObject.CATEGORY_PLACE to "Place",
+            FirebaseVisionObject.CATEGORY_PLANT to "Plant"
+        )
+    }
+}
